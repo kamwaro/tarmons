a1afd63074324134b2b8b474f5b25f101993ad08
kamwaro <petersonkamwaro@gmail.com>
2021-02-26 00:13:05 +0300

    configured react lazy loading

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
+import React, {Suspense} from 'react';
 import 'antd/dist/antd.css'
 import './App.css';
-import Home from './Views/Home'
-import Contact from './Views/Contact'
-import Pork from './Views/Pork'
-import Chicken from './Views/Chicken'
-import Fish from './Views/Fish'
 import {Route,Switch,Redirect} from 'react-router-dom';
+
+const Home = React.lazy(() => import('./Views/Home'))
+const Contact = React.lazy(() => import('./Views/Contact'))
+const Pork = React.lazy(() => import('./Views/Pork'))
+const Chicken = React.lazy(() => import('./Views/Chicken'))
+const Fish = React.lazy(() => import('./Views/Fish'))
+
 
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/home" exact>
-          <Home/>
-        </Route>
-        <Route path="/contact">
-          <Contact/>
-        </Route>
-        <Route path="/pork">
-          <Pork/>
-        </Route>
-        <Route path="/chicken">
-          <Chicken/>
-        </Route>
-        <Route path="/fish">
-          <Fish/>
-        </Route>
+        <React.Suspense fallback={<p>Loading...</p>}>
+        <Route path="/home" exact render={() => <Home/>}></Route>
+        <Route path="/contact" render={() => <Contact/>}></Route>
+        <Route path="/pork" render={() => <Pork/>}></Route>
+        <Route path="/chicken" render={() => <Chicken/>}></Route>
+        <Route path="/fish" render={() =>  <Fish/>}></Route>
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
+        </React.Suspense>
       </Switch>
      
 
